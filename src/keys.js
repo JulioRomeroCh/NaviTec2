b2029fa26e372f6dbfe5a1ec724439c8470aa571
Jose Blanco <joseblanco1313@gmail.com>
2020-12-28 00:40:29 -0600

    Initial

---
+++ src/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '260301',
+        database: 'navitec'
+    }
+};
---
+++ src/routes/links.js
@@ -0,0 +1,15 @@
+const express  = require ('express');
+const router = express.Router ();
+
+const pool = require ('../database');
+
+//Change the name add
+router.get ('/add', (req, res) => {
+    res.render('links/add');
+})
+
+router.post('/add', (req, res) => {
+    res.send ('submit');
+});
+
+module.exports = router;
